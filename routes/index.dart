88db8aca844cbaf966910c64aa1835391e4b1015
iAbhishekPatil <abhipatil1912@gmail.com>
2022-08-28 14:59:55 +0530

    postman collection added

--- routes/index.dart
+++ routes/index.dart
@@ -1,5 +1,20 @@
+import 'dart:io';
+
 import 'package:dart_frog/dart_frog.dart';
 
 Response onRequest(RequestContext context) {
-  return Response(body: 'Welcome to Dart Frog!');
+  return Response(
+    body: htmlString,
+    headers: {HttpHeaders.contentTypeHeader: ContentType.html.value},
+  );
 }
+
+const htmlString = '''
+<!DOCTYPE html>
+<html>
+<body>
+<h1>Manage your Todos</h1>
+<a href="/postman_collection.json"> download postman collection </a>
+</body>
+</html>
+''';
